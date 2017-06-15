4761639220a66d6304b0ed1736ad53cf28049e98
Diego Garcia <desarrolladorweb@tekvia.com.mx>
2017-06-15 11:59:40 -0500

    prueba

--- mercaditoApi/prueba/prueba.php
+++ mercaditoApi/prueba/prueba.php
@@ -1,33 +1,3 @@
 <?php
-  require "../vendor/autoload.php";
-
-
-  $tiempo = date("d")."/".date("m")."/".date("Y")." ".date("g").":".date("i")." ".date("a");
-  echo $tiempo;
-
-  $correo = "diegoalejandroene17@gmail.com";
-  $mail = new PHPMailer();
-  $mail->IsSMTP();
-  $mail->SMTPAuth = true;
-  $mail->SMTPSecure = "ssl";
-  $mail->Host = "smtp.gmail.com";
-  $mail->Port = 465;
-  $mail->Username = "tekviaprogramacion@gmail.com";
-  $mail->Password = "tekvia123";
-
-
-  $mail->setFrom('tekviaprogramacion@gmail.com','Tekvia');
-  $mail->Subject = "Funcion de cronJob";
-  $mail->AltBody = "Esta es la fecha";
-  $mail->MsgHTML("$tiempo");
-  $mail->AddReplyTo("$correo");
-  $mail->AddAddress("$correo");
-  $mail->IsHTML(true);
-  if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-  } else {
-      echo 'Message has been sent';
-  }
-  //wget -q -O /dev/null http://mercaditopuertadelsol.com/mercaditoApi/prueba/prueba.php
+  //cambie esto
 ?>
